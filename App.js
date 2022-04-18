581f51ad43121b5c1094c7cf8358a6db49040b8a
YousefMujahid <70110976+YousefMujahid@users.noreply.github.com>
2022-04-18 00:32:22 -0400

    Runner code

---
+++ App.js
@@ -0,0 +1,13 @@
+//import liraries
+import React, { Component } from 'react';
+import AppNavigation from './src/navigation/AppNavigation'
+
+// create a component
+const App = () => {
+  return (
+   <AppNavigation />
+  );
+};
+
+
+export default App;
